e948a26198d1793d1c0aebd277c8e737d64eccf1
Shira-sun <shira@seri-levi.com>
2021-09-21 21:39:18 -0400

    new final output

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@
 let myTexts = [
   "Your partners are sharing what’s on their minds, and you find that what they’re excited about is compatible with your goals and hopes for the future, as Mercury harmonizes with Jupiter. There are some deep secrets revealed as Mercury clashes with power planet Pluto, giving you a deeper understanding of other people’s fears. The full moon in Pisces gives you insight into a part of yourself that you normally don’t get to witness. Your dreams will definitely be more interesting, so pay attention to what they’re telling you. The sun enters your chart’s house of partnerships and interpersonal commitments! Libra season begins a new chapter in your relationships! Your planetary ruler Mars harmonizes with Saturn, increasing your drive to resolve tough challenges.",
   "This is a restful, familial time of year for you, Cancer. You’re getting to know more about your roots and past. You’re looking into the things that you share with your family as Mercury harmonizes with Jupiter, bringing lessons about your home and upbringing. Mercury clashes with power planet Pluto, giving you insight into how your childhood is repeated in relationships. The full moon in fellow water sign Pisces illuminates your chart’s house of learning and faraway travels, giving you insight into what you want to explore. This connects you to life’s mysteries, which can be a spiritual and faithful moment! Action planet Mars harmonizes with Saturn, the planet of standards and boundaries, which might feel like you’re doing a lot of heavy lifting in a shared task. Ask for a helping hand.",
+  "The full moon in Pisces illuminates a dreamy and future-thinking sector of your chart. This is a time to connect with your objectives and see all the ground you’ve covered. It’s possible that you no longer identify with dreams that you used to have, and are ready to let go of old expectations. Your planetary ruler Venus faces off with unconventional Uranus, so things that you used to want in you relationships may no longer apply now that you have a new edge to be experienced. Libra season will put you in a state of flow as you connect to the rhythm of your daily commitments. This is the perfect time of year to shape your habits in a way that is lasting and pleasant.",
 ];
 let index = 0;
 
@@ -35,4 +36,8 @@ function regExp() {
   );
   console.log(outputText);
   document.getElementById("output").innerHTML = outputText;
+
+  let finaloutputText = text.replace(regex, "");
+  console.log(outputText);
+  document.getElementById("final output").innerHTML = finaloutputText;
 }
